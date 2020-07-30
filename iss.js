6351750129905b56be5ecb4d3c56b6bcf1911df7
Marisa Doig <mcr.doig@gmail.com>
2020-07-30 22:07:55 +0000

    Implemented and checked a function to find my own IP address using ipify API

--- iss.js
+++ iss.js
@@ -1,1 +1,26 @@
+const request = require('request');
 
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    } else {
+      let data = JSON.parse(body);
+      return callback(null, data.ip);
+    }
+  });
+};
+
+module.exports = { fetchMyIP };
